0d61df483b91ddd97a10ee5e574f9d39a3023850
maryam <maryam.nbyn@gmail.com>
2020-05-13 17:23:26 +0430

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Vandar Gravityform
+Plugin URI: https://vandar.io/plugins
+Description: افزونه درگاه پرداخت وندار برای فرم ساز فوق پیشرفته Gravity Forms
+Version: 1.0.0
+Author: گرویتی فرم پارسی
+Author URI: http://vandar.io/
+*/
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+require_once( 'vandar.php' );
